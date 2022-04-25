6448cdf0550bc9d06b37a14002bb1b95608ffe7e
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-04-25 19:57:42 +0300

    Fixing

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 "use strict";
 
 let guitarBody = document.getElementById("guitarBody");
-let input1 = document.getElementById("color1");
+let input1 = document.getElementById("color");
+
+guitarBody.style.fill = 'white';
 
 input1.addEventListener('input', () => {
     guitarBody.style.fill = input1.value;
