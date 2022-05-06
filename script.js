046fdbd585937140143c1554e884def87890ab0e
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-05-06 15:45:22 +0300

    Fixing media

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let guitarBody = document.getElementById("guitarBody");
 let colorInp = document.getElementById("colorInp");
 
 guitarBody.style.fill = 'white';
+guitarBody.focus();
 
 colorInp.addEventListener('input', () => {
     guitarBody.style.fill = colorInp.value;
