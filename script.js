9b7f66417bbbfd2eca8adf6203336ee28d92415d
witcherwow240 <vova.saenko1@gmail.com>
2024-02-04 21:55:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 "use strict";
 
-let guitarBody = document.getElementById("guitarBody");
-let colorInp = document.getElementById("colorInp");
+const guitarBody = document.getElementById("guitarBody");
+const colorInp = document.getElementById("colorInp");
+const griff = document.getElementById("griff");
 
 guitarBody.style.fill = 'white';
 
 colorInp.addEventListener('input', () => {
     guitarBody.style.fill = colorInp.value;
+    griff.style.fill = colorInp.value;
 })
